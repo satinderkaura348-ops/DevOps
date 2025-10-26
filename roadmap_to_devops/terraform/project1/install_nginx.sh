0c798d55b5933a9c12a631f2e0745d10fd73df3e
satinder <satinderkaura348@gmail.com>
2025-10-26 17:47:26 +1100

    bug

--- roadmap_to_devops/terraform/project1/install_nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update 
-sudo apt-get install nginx -y
-sudo systemctl start nginx
-sudo systemctl enable nginx
-
-echo "<h1> This is to confirm if shell script worked and nginx installed</h1>" | sudo tee /var/www/html/index.html
